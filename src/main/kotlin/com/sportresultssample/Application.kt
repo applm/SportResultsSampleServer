49cbb9b44ab555ae4b390606a5128b9d18f69b42
Martin Appl <mappl@smithmicro.com>
2022-11-29 14:05:46 +0100

    Remove unnecessary stuff

--- src/main/kotlin/com/sportresultssample/Application.kt
+++ src/main/kotlin/com/sportresultssample/Application.kt
@@ -1,12 +1,10 @@
 package com.sportresultssample
 
 import com.apurebase.kgraphql.GraphQL
-import com.apurebase.kgraphql.KGraphQL
+import com.sportresultssample.schema.sportResultSchema
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.sportresultssample.plugins.*
-import com.sportresultssample.schema.sportResultSchema
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -15,7 +13,6 @@ fun main() {
 
 fun Application.module(testing: Boolean = false) {
     install(GraphQL) {
-        configureRouting()
         playground = true
         schema {
             sportResultSchema()
--- src/main/kotlin/com/sportresultssample/data/Database.kt
+++ src/main/kotlin/com/sportresultssample/data/Database.kt
@@ -2,4 +2,30 @@ package com.sportresultssample.data
 
 import com.sportresultssample.model.SportResult
 
-val sportResults = mutableMapOf<String, SportResult>()
+val sportResults = testInitSportResults()
+
+fun testInitSportResults() : MutableMap<String, SportResult> {
+    val sr = mutableMapOf<String, SportResult>()
+    var id = java.util.UUID.randomUUID().toString()
+    sr[id] = SportResult(
+        uid = id,
+        name = "Running",
+        place = "Berlin",
+        duration = "1:30"
+    )
+    id = java.util.UUID.randomUUID().toString()
+    sr[id] = SportResult(
+        uid = id,
+        name = "Swimming",
+        place = "Hamburg",
+        duration = "2:30"
+    )
+    id = java.util.UUID.randomUUID().toString()
+    sr[id] = SportResult(
+        uid = id,
+        name = "Cycling",
+        place = "Munich",
+        duration = "3:30"
+    )
+    return sr
+}
